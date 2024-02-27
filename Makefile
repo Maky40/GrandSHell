059e4ac1e06671396328568215c7e90c62a1b056
Maxime Nie <mnie@c2r1p5.42perpignan.fr>
2024-02-27 14:30:16 +0100

    MN -- fichier + fct dup_env

--- Makefile
+++ Makefile
@@ -22,7 +22,7 @@ GNL_FILE	=	gnl.a
 
 GNL_LIB	=	$(addprefix $(GNL_PATH), $(GNL_FILE))
 
-C_FILE		=	main.c
+C_FILE		=	main.c dup_env.c
 
 SRC_DIR		=	./src/
 
--- src/dup_env.c
+++ src/dup_env.c
@@ -1,30 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   dup_env.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mnie <mnie@student.42perpignan.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/26 11:14:57 by mnie              #+#    #+#             */
-/*   Updated: 2024/02/27 14:12:32 by mnie             ###   ########.fr       */
+/*   Created: 2024/02/27 14:03:53 by mnie              #+#    #+#             */
+/*   Updated: 2024/02/27 14:09:34 by mnie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int main(int argc, char **argv, char **envp)
+void	free_dup_env(char **dup_env)
 {
-	char	**env;
 	int		i;
 
-	(void) argc;
-	(void) argv;
-	env = dup_env(envp);
 	i = 0;
-	while (env[i])
+	while (dup_env[i])
 	{
-		ft_printf("%s\n", env[i]);
+		free(dup_env[i]);
 		i++;
 	}
-	return (0);
+	free(dup_env);
+	dup_env = NULL;
+	return ;
+}
+
+char 	**dup_env(char **envp)
+{
+	int		i;
+	char	**env_dup;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	env_dup = malloc(sizeof(char *) * (i + 1));
+	i = 0;
+	while (envp[i])
+	{
+		env_dup[i] = ft_strdup(envp[i]);
+		i++;
+	}
+	env_dup[i] = NULL;
+	return (env_dup);
 }
