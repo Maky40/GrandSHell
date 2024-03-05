3488a83690bb61698e7c00d6c089eab018516ea5
Xavier Berger <xavier.berger66@gmail.com>
2024-03-05 17:29:46 +0100

    XB- expander with memcpy instead of strncpy

--- src/expander.c
+++ src/expander.c
@@ -6,11 +6,25 @@
 /*   By: xav <xav@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 10:21:17 by xav               #+#    #+#             */
-/*   Updated: 2024/03/05 15:47:00 by xav              ###   ########.fr       */
+/*   Updated: 2024/03/05 17:19:39 by xav              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+char *ft_strcat(char *dest, const char *src)
+{
+    char *ptr;
+	
+	ptr = dest + ft_strlen(dest);
+    while (*src != '\0')
+    {
+        *ptr++ = *src++;
+    }
+    *ptr = '\0';
+    return dest;
+}
+
 
 char	*ft_strncpy(char *dest, char *src, size_t n)
 {
@@ -32,52 +46,56 @@ char	*ft_strncpy(char *dest, char *src, size_t n)
 
 void expand_variable(t_data *data, t_lexer *dup)
 {
-	char *ptr;
-	char *var_start;
-	char *var_end;
-	char *var_name;
-	size_t var_len;
+    char *ptr;
+    char *var_start;
+    char *var_end;
+    char *var_name;
+    size_t var_len;
 
-	(void)data;
-	ptr = dup->str;
-	while (*ptr)
-	{
-		if (*ptr == '$' && ptr[1] != '\0')
-		{
-			var_start = ptr + 1;
-			var_end = var_start;
-			while(*var_end != ' ' && *var_end != '$' && *var_end && *var_end != '"') 
-				var_end++;
-			var_len = var_end - var_start;
-			var_name = malloc(sizeof(char *) *(var_len + 1));
-			if (var_name)
-			{
-				ft_strncpy(var_name, var_start, var_len);
-				var_name[var_len] = '\0';
-				printf("%s\n", var_name);	
-			}
-			char *var_value = getenv(var_name);
+    (void)data;
+    ptr = dup->str;
+    while (*ptr)
+    {
+        if (*ptr == '$' && ptr[1] != '\0')
+        {
+            var_start = ptr + 1;
+            var_end = var_start;
+            while(*var_end != ' ' && *var_end != '$' && *var_end && *var_end != '"') 
+                var_end++;
+            var_len = var_end - var_start;
+            var_name = malloc(var_len + 1);
+            if (var_name)
+            {
+                memcpy(var_name, var_start, var_len);
+                var_name[var_len] = '\0';    
+            }
+            char *var_value = getenv(var_name); // Récupérer la valeur de la variable d'environnement
+            printf("Env : %s\n", var_value);
 			if (var_value != NULL)
-			{
-				size_t str_len = strlen(dup->str);
-				size_t new_len = str_len - (var_end - var_start) + strlen(var_value);
-				char *new_str = malloc(new_len + 1);
-				if (new_str)
-				{
-					strncpy(new_str, dup->str, var_start - dup->str - 1); // Copie la partie avant la variable
-					strcat(new_str, var_value); // Concatène la valeur de la variable
-					strcat(new_str, var_end); // Concatène la partie après la variable
-					free(dup->str); // Libère l'ancien contenu de dup->str
-					dup->str = new_str; // Remplace le contenu de dup->str par le nouveau tampon
-					printf("%s\n", dup->str);
-					ptr = new_str;
-				}
-			}
-			free(var_name);
-		}
-		ptr++;
-	}
-	}
+            {
+                size_t str_len = strlen(dup->str);
+                size_t var_start_index = var_start - dup->str - 1;
+                size_t var_end_index = var_end - dup->str;
+                size_t new_len = str_len - var_len + strlen(var_value);
+                char *new_str = malloc(new_len + 1);
+                if (new_str)
+                {
+                    ft_memcpy(new_str, dup->str, var_start_index); // Copier la partie avant la variable
+                    strcpy(new_str + var_start_index, var_value); // Concaténer la valeur de la variable
+                    strcat(new_str, dup->str + var_end_index); // Concaténer la partie après la variable
+                    free(dup->str); // Libérer l'ancien contenu de dup->str
+                    dup->str = new_str; // Remplacer le contenu de dup->str par le nouveau tampon
+                   // printf("%s\n", dup->str);
+                    ptr = new_str;
+                }
+            }
+            free(var_name);
+        }
+        ptr++;
+    }
+		printf("%s\n", dup->str);
+}
+
 
 /*
 void handle_multiple_expand(t_data *data, t_lexer *dup)
