c5e9ff5b60aaa372b8507044a2ff5e003c0836b5
Xavier Berger <xavier.berger66@gmail.com>
2024-03-07 15:30:22 +0100

    XB - expander files normed

--- src/expander.c
+++ src/expander.c
@@ -6,35 +6,34 @@
 /*   By: xav <xav@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 10:21:17 by xav               #+#    #+#             */
-/*   Updated: 2024/03/07 14:47:30 by xav              ###   ########.fr       */
+/*   Updated: 2024/03/07 15:16:29 by xav              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-
-char *get_env_value(char **envp, char *var_name)
+char	*get_env_value(char **envp, char *var_name)
 {
-    while (*envp != NULL)
-    {
-        if (ft_strncmp(*envp, var_name, ft_strlen(var_name)) == 0 && (*envp)[ft_strlen(var_name)] == '=')
-            return (*envp + ft_strlen(var_name) + 1);
-        envp++;
-    }
-    return NULL;
+	while (*envp != NULL)
+	{
+		if (ft_strncmp(*envp, var_name, ft_strlen(var_name)) == 0
+			&& (*envp)[ft_strlen(var_name)] == '=')
+			return (*envp + ft_strlen(var_name) + 1);
+		envp++;
+	}
+	return (NULL);
 }
 
-
-void find_expander_len(char *ptr, t_expander *expander) 
+void	find_expander_len(char *ptr, t_expander *expander)
 {
-    if (ptr[1] > '0' && ptr[1] <= '9')
-        expander->start = ptr + 2;
-    else    
-        expander->start = ptr + 1;
-    expander->end = expander->start;
-    while (*expander->end != ' ' && *expander->end != '$' 
-	&& *expander->end && *expander->end != '"') 
-        expander->end++;
+	if (ptr[1] > '0' && ptr[1] <= '9')
+		expander->start = ptr + 2;
+	else
+		expander->start = ptr + 1;
+	expander->end = expander->start;
+	while (*expander->end != ' ' && *expander->end != '$'
+		&& *expander->end && *expander->end != '"')
+		expander->end++;
 }
 
 void	get_value(t_data *data, t_lexer *dup, t_expander *expander, char **ptr)
@@ -44,54 +43,52 @@ void	get_value(t_data *data, t_lexer *dup, t_expander *expander, char **ptr)
 	if (expander->name)
 	{
 		ft_memcpy(expander->name, expander->start, expander->len);
-		expander->name[expander->len] = '\0'; 
+		expander->name[expander->len] = '\0';
 	}
 	if (expander->name[0] == '?')
-        expander->value = ft_itoa(data->exit_status);
-    else if (expander->name[0] > '0' && expander->name[0] <= '9')
-        expander->value = expander->name;
-    else
+		expander->value = ft_itoa(data->exit_status);
+	else if (expander->name[0] > '0' && expander->name[0] <= '9')
+		expander->value = expander->name;
+	else
 	{
-    	expander->value = get_env_value(data->env, expander->name);
+		expander->value = get_env_value(data->env, expander->name);
 	}
 	if (expander->value == NULL)
-		return;
+		return ;
 	if (expander->name[0] > '0' && expander->name[0] <= '9')
 		new_str_number(dup, expander, ptr);
 	else
 		new_str(dup, expander, ptr);
 }
 
-
-
-void expand_variable(t_data *data, t_lexer *dup)
+void	expand_variable(t_data *data, t_lexer *dup)
 {
-    char *ptr;
-    t_expander expander;
+	char		*ptr;
+	t_expander	expander;
 
-    ptr = dup->str;
-    while (*ptr)
-    {   
-        if (*ptr == '$' && (ptr[1] != '\0' && ptr[1] != ' ' && ptr[1] != '.'))
-        {
+	ptr = dup->str;
+	while (*ptr)
+	{
+		if (*ptr == '$' && (ptr[1] != '\0' && ptr[1] != ' ' && ptr[1] != '.'))
+		{
 			find_expander_len(ptr, &expander);
 			get_value(data, dup, &expander, &ptr);
-            if (expander.value == NULL)
-            {
-                if (dup->str[0] == '"')
+			if (expander.value == NULL)
+			{
+				if (dup->str[0] == '"')
 					new_str_null(dup, &expander, &ptr);
-                else
-					dup->str = NULL;            
-            }
-    	}
-        ptr++;
+				else
+					dup->str = NULL;
+			}
+		}
+		ptr++;
 	}
-    printf("dup->str : %s\n", dup->str);
+	printf("dup->str : %s\n", dup->str);
 }
 
 void	expander(t_data *data, t_lexer **lexer)
 {
-	t_lexer	*dup; 
+	t_lexer	*dup;
 
 	dup = *lexer;
 	while (dup)
--- src/expander_utils.c
+++ src/expander_utils.c
@@ -6,103 +6,104 @@
 /*   By: xav <xav@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 14:44:56 by xav               #+#    #+#             */
-/*   Updated: 2024/03/07 14:47:05 by xav              ###   ########.fr       */
+/*   Updated: 2024/03/07 15:29:07 by xav              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-char *ft_strcpy(char *dest, const char *src)
-
+char	*ft_strcpy(char *dest, const char *src)
 {
-    char *ptr = dest;
-    while (*src)
-    {
-        *ptr++ = *src++;
-    }
-    *ptr = '\0';
-    return dest;
+	char	*ptr;
+
+	ptr = dest;
+	while (*src)
+	{
+		*ptr++ = *src++;
+	}
+	*ptr = '\0';
+	return (dest);
 }
 
-char *ft_strcat(char *dest, const char *src)
+char	*ft_strcat(char *dest, const char *src)
 {
-    char *ptr;
-	
+	char	*ptr;
+
 	ptr = dest + ft_strlen(dest);
-    while (*src != '\0')
-    {
-        *ptr++ = *src++;
-    }
-    *ptr = '\0';
-    return dest;
+	while (*src != '\0')
+	{
+		*ptr++ = *src++;
+	}
+	*ptr = '\0';
+	return (dest);
 }
-void new_str_null(t_lexer *dup, t_expander *expander, char **ptr)
+
+void	new_str_null(t_lexer *dup, t_expander *expander, char **ptr)
 {
-    size_t str_len;
-    size_t start_index;
-    size_t end_index;
-    size_t new_len;
-    char *new_str;
+	size_t	str_len;
+	size_t	start_index;
+	size_t	end_index;
+	size_t	new_len;
+	char	*new_str;
 
 	str_len = ft_strlen(dup->str);
 	start_index = expander->start - dup->str - 1;
 	end_index = expander->end - dup->str;
 	new_len = str_len - expander->len;
-	new_str = malloc(sizeof(char *) * (new_len + 1));	
-    if (!new_str)
+	new_str = malloc(sizeof(char *) * (new_len + 1));
+	if (!new_str)
 		return ;
-    ft_memcpy(new_str, dup->str, start_index);
-    ft_strcat(new_str, dup->str + end_index); 
-    free(dup->str); 
-    dup->str = new_str; 
-    printf("dup->str : %s\n", dup->str);
-    *ptr = new_str;
+	ft_memcpy(new_str, dup->str, start_index);
+	ft_strcat(new_str, dup->str + end_index);
+	free(dup->str);
+	dup->str = new_str;
+	printf("dup->str : %s\n", dup->str);
+	*ptr = new_str;
 }
 
-void new_str(t_lexer *dup, t_expander *expander, char **ptr)
+void	new_str(t_lexer *dup, t_expander *expander, char **ptr)
 {
-    size_t str_len;
-    size_t start_index;
-    size_t end_index;
-    size_t new_len;
-    char *new_str;
+	size_t	str_len;
+	size_t	start_index;
+	size_t	end_index;
+	size_t	new_len;
+	char	*new_str;
 
 	str_len = ft_strlen(dup->str);
 	start_index = expander->start - dup->str - 1;
 	end_index = expander->end - dup->str;
 	new_len = str_len - expander->len + ft_strlen(expander->value);
-	new_str = malloc(sizeof(char *) * (new_len + 1));	
-    if (!new_str)
+	new_str = malloc(sizeof(char *) * (new_len + 1));
+	if (!new_str)
 		return ;
-    ft_memcpy(new_str, dup->str, start_index); 
-    ft_strcpy(new_str + start_index, expander->value); 
-    ft_strcat(new_str, dup->str + end_index); 
-    free(dup->str); 
-    dup->str = new_str; 
-    *ptr = new_str;
+	ft_memcpy(new_str, dup->str, start_index);
+	ft_strcpy(new_str + start_index, expander->value);
+	ft_strcat(new_str, dup->str + end_index);
+	free(dup->str);
+	dup->str = new_str;
+	*ptr = new_str;
 }
 
-
-void new_str_number(t_lexer *dup, t_expander *expander, char **ptr)
+void	new_str_number(t_lexer *dup, t_expander *expander, char **ptr)
 {
-    size_t str_len;
-    size_t start_index;
-    size_t end_index;
-    size_t new_len;
-    char *new_str;
+	size_t	str_len;
+	size_t	start_index;
+	size_t	end_index;
+	size_t	new_len;
+	char	*new_str;
 
 	str_len = ft_strlen(dup->str);
 	start_index = expander->start - dup->str - 2;
 	end_index = expander->end - dup->str;
 	new_len = str_len - expander->len + ft_strlen(expander->value);
 	new_str = malloc(sizeof(char *) * (new_len + 1));
-    if (!new_str)
-		return ; 
-    ft_memcpy(new_str, dup->str, start_index); 
-    ft_strcpy(new_str + start_index, expander->value); 
-    ft_strcat(new_str, dup->str + end_index); 
-    free(dup->str);
-    dup->str = new_str; 
-    printf("dup->str : %s\n", dup->str);
+	if (!new_str)
+		return ;
+	ft_memcpy(new_str, dup->str, start_index);
+	ft_strcpy(new_str + start_index, expander->value);
+	ft_strcat(new_str, dup->str + end_index);
+	free(dup->str);
+	dup->str = new_str;
+	printf("dup->str : %s\n", dup->str);
 	*ptr = new_str;
 }
