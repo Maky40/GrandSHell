31b3aa5a64e57e74c64faa7f100b2178ac6e7a24
Xavier Berger <xavier.berger66@gmail.com>
2024-03-10 10:16:02 +0100

    XB - builtin check fixed

--- src/executor.c
+++ src/executor.c
@@ -6,7 +6,7 @@
 /*   By: xav <xav@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 15:18:42 by xav               #+#    #+#             */
-/*   Updated: 2024/03/09 10:51:47 by xav              ###   ########.fr       */
+/*   Updated: 2024/03/09 13:14:59 by xav              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,21 @@ void execute(t_command *cmd, t_data *data)
 
 void	built_in_execute(t_command *cmd, t_data *data)
 {
-	if (check_command(cmd, "echo"))
-		echo();
-	else if (check_command(cmd, "unset"))
-		unset();
-	else if (check_command(cmd, "cd"))
-		cd();
-	else if (check_command(cmd, "exit"))
-		ft_exit();
-	else if (check_command(cmd, "pwd"))
-		pwd();
-	else if (check_command(cmd, "env"))
-		env();
-	else if (check_command(cmd, "export"))
-		export();
+	(void)data;
+	if (check_command(cmd->command, "echo"))
+		printf("echo\n");
+	else if (check_command(cmd->command, "unset"))
+		printf("unset\n");
+	else if (check_command(cmd->command, "cd"))
+		printf("cd\n");
+	else if (check_command(cmd->command, "exit"))
+		printf("exit\n");
+	else if (check_command(cmd->command, "pwd"))
+		printf("pwd\n");
+	else if (check_command(cmd->command, "env"))
+		printf("env\n");
+	else if (check_command(cmd->command, "export"))
+		printf("export\n");
 }
 
 int	is_builtin(char *cmd)
@@ -74,7 +75,19 @@ void executor(t_table *tab_cmds, t_data *data)
 	int	i; 
 
 	i = 0;
-
+	if (tab_cmds->num_commands == 1)
+	{
+		if (tab_cmds->commands[0].input_file == NULL)
+			tab_cmds->commands[0].input_file = STDIN_FILENO;
+		if (tab_cmds->commands[0].output_file == NULL)
+			tab_cmds->commands[0].input_file = STDOUT_FILENO;
+	}
+	else if (tab_cmds->num_commands > 1)
+	{
+		if (tab_cmds->commands[tab_cmds->num_commands - 1].output_file ==  NULL)
+			tab_cmds->commands[tab_cmds->num_commands - 1].output_file = STDOUT_FILENO;
+			
+	}
 	while (i < tab_cmds->num_commands)
 	{
 		if (is_builtin(tab_cmds->commands[i].command) == 0)
