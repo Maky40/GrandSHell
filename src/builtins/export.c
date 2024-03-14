831c9dc6aa9e300131d9c88c83a6e9ef1537710a
Maky40 <nie.maxime40@gmail.com>
2024-03-14 14:54:06 +0100

    MN - ajout des quotes dans export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,11 +6,37 @@
 /*   By: mnie <mnie@student.42perpignan.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 12:48:47 by mnie              #+#    #+#             */
-/*   Updated: 2024/03/14 14:38:31 by mnie             ###   ########.fr       */
+/*   Updated: 2024/03/14 14:50:37 by mnie             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+char	*add_line_quote(char **env_tmp, int i)
+{
+	char	*new_line;
+	int		len;
+	int		j;
+
+	len = ft_strlen(env_tmp[i]) + 2;
+	new_line = malloc(sizeof(char) * len + 1);
+	while (env_tmp[i][j] != '=')
+	{
+		new_line[j] = env_tmp[i][j];
+		j++;
+	}
+	new_line[j] = '"';
+	j++;
+	while (env_tmp[i][j])
+	{
+		new_line[j] = env_tmp[i][j];
+		j++;
+	}
+	new_line[j] = '"';
+	new_line[j + 1] = "\n";
+	free(env_tmp[i]);
+	return(new_str);
+}
 
 void	export_simple(t_command *cmd, t_data *data)
 {
@@ -29,6 +55,13 @@ void	export_simple(t_command *cmd, t_data *data)
 			env_tmp[i] = swap;
 			i = -1;
 		}
+		i++;
+	}
+	i = 0;
+	while (env_tmp[i])
+	{
+		env_tmp[i] = add_line_quote(env_tmp, i);
+		ft_printf("declare -x %s\n");
 		i++;
 	}
 	free_dup_env(env_tmp);
